9a28b9ba67073af7660c08fb1575bae51c16608f
camendoza2000 <camendoza@sciodev.com>
2015-01-29 04:29:53 -0600

    broken JS funtionality

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
-'use strict';
+// 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.show',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/show'});
-}]);
+// // Declare app level module which depends on views, and components
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.show',
+//   'myApp.view2',
+//   'myApp.version'
+// ]).
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.otherwise({redirectTo: '/show'});
+// }]);
+
+(function(){
+	var app = angular.module("myApp", []);
+	app.controller('showUsersController', function(){
+		this.users = data;
+		
+	});
+
+	var data = angular.fromJson(app/user_data/user_data.json);
+
+})();

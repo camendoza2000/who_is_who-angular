9219d8d89769920f4ca2cc005f57614dc8b1f9b6
Carlos Mendoza <camendoza@sciodev.com>
2015-01-29 22:01:29 +0000

    again show user name, nice app.js file

--- app/app.js
+++ app/app.js
@@ -1,31 +1,16 @@
-// 'use strict';
 
-// // Declare app level module which depends on views, and components
-// angular.module('myApp', [
-//   'ngRoute',
-//   'myApp.show',
-//   'myApp.view2',
-//   'myApp.version'
-// ]).
-// config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.otherwise({redirectTo: '/show'});
-// }]);
+(function() {
+  var app = angular.module('myApp', []);
 
-var app = angular.module("myApp", []);
-	
- (function(){
-	app.controller('showUsersController', function(){
-		this.users = data
-		
-	});
+  app.controller('ShowUserController', function(){
+    this.users = data;
+  });
 
-	// var data = angular.fromJson(app/user_data/user_data.json);
-
-   var data= [
-				{name: "Pancho", avatar: "app/images/1.jpg", interests: "punch giraffes", id: "1"},
-				{name: "Maria", avatar: "app/images/2.jpg", interests: "flagelarse", id: "2"},
-				{name: "Concha", avatar: "app/images/3.jpg", interests: "ser una loncha de queso", id: "3"},
-				{name: "Laura", avatar: "app/images/4.jpg", interests: "nadar", id: "4"}
-			];
-
- })();
+  var data = [
+    {name: "Pancho", avatar: "app/images/1.jpg", interests: "punch giraffes", id: "1" },
+    {name: "Maria", avatar: "app/images/2.jpg", interests: "flagelarse", id: "2"},
+  	{name: "Concha", avatar: "app/images/3.jpg", interests: "ser una loncha de queso", id: "3"},
+  	{name: "Laura", avatar: "app/images/4.jpg", interests: "nadar", id: "4"}
+  ];
+  
+})();

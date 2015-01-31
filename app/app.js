933bc47283bf0789322e147a99e8071697249352
Carlos Mendoza <camendoza@sciodev.com>
2015-01-31 00:44:48 +0000

    try call data from Json, but fails

--- app/app.js
+++ app/app.js
@@ -6,11 +6,13 @@
     this.users = data;
   });
 
-  var data = [
-    {name: "Pancho", avatar: "images/1.jpg", interests: "punch giraffes", id: "1" },
-    {name: "Maria", avatar: "images/2.jpg", interests: "flagelarse", id: "2"},
-  	{name: "Concha", avatar: "images/3.jpg", interests: "ser una loncha de queso", id: "3"},
-  	{name: "Laura", avatar: "images/4.jpg", interests: "nadar", id: "4"}
-  ];
+// var data = [
+//   {name: "Pancho", avatar: "images/1.jpg", interests: "punch giraffes", id: "1" },
+//   {name: "Maria", avatar: "images/2.jpg", interests: "flagelarse", id: "2"},
+// 	{name: "Concha", avatar: "images/3.jpg", interests: "ser una loncha de queso", id: "3"},
+// 	{name: "Laura", avatar: "images/4.jpg", interests: "nadar", id: "4"}
+// ];
+
+  var data = angular.fromJson(user_data/user_data.json);
   
 })();

57aeadfd70adb3edf542f3c36a349b6a7f67968f
camendoza2000 <camendoza@sciodev.com>
2015-02-02 03:37:46 -0600

    ng-model to search

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@
  var data = [
   {name: "Pancho", avatar: "images/1.jpg", interests: "punch giraffes", id: "1" },
   {name: "Maria", avatar: "images/2.jpg", interests: "flagelarse", id: "2"},
-  {name: "Concha", avatar: "images/3.jpg", interests: "ser una loncha de queso", id: "3"},
+  {name: "Concha", avatar: "images/3.jpg", interests: "Ser una loncha de queso", id: "3"},
   {name: "Laura", avatar: "images/4.jpg", interests: "nadar", id: "4"}
  ];
 

f4ef4cd66cb1c89caf6f73ae5b3f09a852c7e283
camendoza2000 <camendoza@sciodev.com>
2015-02-03 03:15:57 -0600

    beatiful changes of files

---
+++ app/js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* App Module */
+
+var app = angular.module('myApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+
+myApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,10 +8,7 @@
     });
   }])
 
-  /*app.controller('ShowInterestController', function(){
-  	this.interests = interests_data;
-  });*/
-
+  
   app.controller('ShowInterestController', ['$scope', '$http', function($scope, $http){
     $http.get("data/interest_data.json").success(function(intetests_data){
       $scope.interests = intetests_data;
@@ -19,13 +16,5 @@
   }])
 
 
-/* var interests_data = [
-   {name: "punch giraffes", user_id: ["1"], id: "1"},
-   {name: "flagelarse", user_id: ["2"], id: "2"},
-   {name: "Ser una loncha de queso", user_id: ["2", "3"], id: "3"},
-   {name: "nadar", user_id: ["4"], id: "4"}			
- ];*/
-
-//  var data = angular.fromJson("user_data/user_data.json");
 
 })();

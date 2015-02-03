503b4e42ac9323155278a36752b7ca087118c2ea
camendoza2000 <camendoza@sciodev.com>
2015-02-03 01:59:02 -0600

    call user's data from JSON

--- app/app.js
+++ app/app.js
@@ -2,20 +2,27 @@
 (function() {
   var app = angular.module('myApp', []);
 
-  app.controller('ShowUserController', function(){
+  /*app.controller('ShowUserController', function(){
     this.users = users_data;
-  });
+  });*/
+
+  app.controller('ShowUserController', ['$scope', '$http', function($scope, $http){
+    $http.get("data/user_data.json").success(function(data){
+      $scope.users = data;
+    });
+  }])
 
   app.controller('ShowInterestController', function(){
   	this.interests = interests_data;
   });
 
- var users_data = [
+
+/* var users_data = [
   {name: "Pancho", avatar: "images/1.jpg", id: "1" },
   {name: "Maria", avatar: "images/2.jpg", id: "2"},
   {name: "Concha", avatar: "images/3.jpg", id: "3"},
   {name: "Laura", avatar: "images/4.jpg", id: "4"}
- ];
+ ];*/
 
  var interests_data = [
    {name: "punch giraffes", user_id: ["1"], id: "1"},
